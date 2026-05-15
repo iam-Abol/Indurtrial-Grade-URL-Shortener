db37cde31b8978620bbab171d42ab2dcbb28563c
unknown <mh.z77w@gmail.com>
2026-05-16 00:28:19 +0330

    feat(database): user entity

--- app/src/database/database.module.ts
+++ app/src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-//test
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
---
+++ app/src/modules/user/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password_hash: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn({ nullable: true })
+  deleted_at: Date;
+}
