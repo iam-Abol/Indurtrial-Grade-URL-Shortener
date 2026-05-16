4689ef2fc6ea2ca5c013c12ccd096236629f9eaa
unknown <mh.z77w@gmail.com>
2026-05-16 18:53:29 +0330

    feat: implement Base62Converter skeleton

---
+++ app/src/common/utils/base62.converter.ts
@@ -0,0 +1,13 @@
+export class Base62Converter {
+  private static readonly ALPHABET =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  private static readonly BASE = 62;
+
+  static encode(num: number): string {
+    return '';
+  }
+
+  static decode(str: string): number {
+    return 0;
+  }
+}
