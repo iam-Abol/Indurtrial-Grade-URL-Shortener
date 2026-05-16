a85be02528c49f6f2b6267d4fe9552d3aa24d66c
unknown <mh.z77w@gmail.com>
2026-05-16 23:14:31 +0330

    feat(url.repository): implement required methods

--- app/src/modules/url/url.repository.ts
+++ app/src/modules/url/url.repository.ts
@@ -9,4 +9,20 @@ export class UrlRepository {
     @InjectRepository(Url)
     private readonly repo: Repository<Url>,
   ) {}
+  async create(url: Partial<Url>): Promise<Url> {
+    const entity = this.repo.create(url);
+    return this.repo.save(entity);
+  }
+  async findByShortCode(shortCode: string) {
+    return this.repo.findOne({ where: { shortCode } });
+  }
+  async updateShortCode(id: number, shortCode: string) {
+    return this.repo.update(id, { shortCode });
+  }
+  async incrementClickCount(id: number) {
+    await this.repo.increment({ id }, 'click_count', 1);
+  }
+  async findByCustomAlias(alias: string) {
+    return this.repo.findOne({ where: { customAlias: alias } });
+  }
 }
