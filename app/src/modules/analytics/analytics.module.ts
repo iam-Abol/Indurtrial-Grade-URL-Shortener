08ee2cdd2fba680f17f19482e14ad93be47b3a69
unknown <mh.z77w@gmail.com>
2026-05-16 22:23:06 +0330

    feat: add entities to typeorm.forfeature

--- app/src/modules/analytics/analytics.module.ts
+++ app/src/modules/analytics/analytics.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
+import { ClickAnalytics } from './entities/analytics.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [AnalyticsService]
+  imports: [TypeOrmModule.forFeature([ClickAnalytics])],
+  providers: [AnalyticsService],
 })
 export class AnalyticsModule {}
