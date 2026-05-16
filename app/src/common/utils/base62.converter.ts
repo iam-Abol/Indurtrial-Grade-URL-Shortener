5ff36d2372d77073538f33bc42e4cd974c512683
unknown <mh.z77w@gmail.com>
2026-05-16 19:38:08 +0330

    feat: implement encode and decode

--- app/src/common/utils/base62.converter.ts
+++ app/src/common/utils/base62.converter.ts
@@ -4,10 +4,25 @@ export class Base62Converter {
   private static readonly BASE = 62;
 
   static encode(num: number): string {
-    return '';
+    let result: string = '';
+    if (num < 0) throw new Error('Invalid inputs');
+    if (num === 0) return '0';
+    while (num > 0) {
+      result = this.ALPHABET[num % this.BASE] + result;
+      num = Math.floor(num / this.BASE);
+    }
+
+    return result;
   }
 
   static decode(str: string): number {
-    return 0;
+    let result: number = 0;
+    for (const char of str) {
+      const index = this.ALPHABET.indexOf(char);
+      if (index === -1) throw new Error('Invalid Base62 character');
+
+      result = result * this.BASE + index;
+    }
+    return result;
   }
 }
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Base62Converter } from './common/utils/base62.converter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(Base62Converter.encode(125));
+
+  console.log(Base62Converter.decode('21'));
 }
 bootstrap();
