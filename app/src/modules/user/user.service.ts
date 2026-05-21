5a303853b1ecd59e6994df590c2270c7d77b8d8e
unknown <mh.z77w@gmail.com>
2026-05-21 22:21:54 +0330

    feat(user): implement create and use bcrypt for hashing

--- app/src/modules/user/user.service.ts
+++ app/src/modules/user/user.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,5 +17,30 @@ export class UserService {
 
   async findByEmail(email: string) {
     return this.repo.findOne({ where: { email } });
+  }
+
+  async create(email: string, password: string) {
+    const SALT_ROUNDS = 12;
+    const existingUser = await this.repo.findOne({
+      where: { email },
+      select: ['id'],
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email already in use');
+    }
+
+    const password_hash = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const user = this.repo.create({
+      email,
+      password_hash,
+    });
+
+    try {
+      return await this.repo.save(user);
+    } catch (err) {
+      throw new InternalServerErrorException('Failed to create user');
+    }
   }
 }
