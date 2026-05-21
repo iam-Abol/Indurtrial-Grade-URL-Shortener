2b42d9891587be6c72f9604fe90420a54663b8a7
unknown <mh.z77w@gmail.com>
2026-05-21 22:12:05 +0330

    feat(user): implement findByEmail

--- app/src/modules/user/user.service.ts
+++ app/src/modules/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private repo: Repository<User>,
+  ) {}
+
+  async findByEmail(email: string) {
+    return this.repo.findOne({ where: { email } });
+  }
+}
