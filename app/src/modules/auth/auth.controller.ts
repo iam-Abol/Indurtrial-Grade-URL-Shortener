e2e9469201feeb370ca073dfed9fb3d68a80bcc5
unknown <mh.z77w@gmail.com>
2026-05-23 19:55:15 +0330

    feat(auth): add dtos for login and signup

--- app/src/modules/auth/auth.controller.ts
+++ app/src/modules/auth/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async signup(@Body() body) {
+  async signup(@Body() body: SignupDto) {
     return this.authService.signup(body.email, body.password);
   }
 
   @Post('login')
-  async login(@Body() body) {
+  async login(@Body() body: LoginDto) {
     return this.authService.login(body.email, body.password);
   }
 }
