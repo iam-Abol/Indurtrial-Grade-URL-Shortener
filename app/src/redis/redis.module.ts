592fabb1519b27c9284715d7737ca40d396dda1a
unknown <mh.z77w@gmail.com>
2026-05-25 00:17:14 +0330

    connect to redis in module

--- app/src/redis/redis.module.ts
+++ app/src/redis/redis.module.ts
@@ -1,4 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisService } from './redis.service';
+import { createClient } from 'redis';
 
-@Module({})
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: async (configService: ConfigService) => {
+        const client = createClient({
+          url:
+            configService.get<string>('REDIS_URL') || 'redis://localhost:6379',
+        });
+
+        client.on('error', (err) => {
+          console.error('Redis Client Error', err);
+        });
+
+        await client.connect();
+        return client;
+      },
+      inject: [ConfigService],
+    },
+    RedisService,
+  ],
+  exports: ['REDIS_CLIENT', RedisService],
+})
 export class RedisModule {}
